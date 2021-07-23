d2cc8c0310ef6849f132aebe4df8e2eb0f55085a
Crystal Anderson <cryssann@gmail.com>
2021-07-22 21:29:29 -0600

    Fixed minor bug with breed list in SearchParams

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,25 +1,25 @@
 import { React, useEffect, useState } from "react";
-import Results from "./Results";
 import useBreedList from "./useBreedList";
+import Results from "./Results";
 
-const ANIMALS = ["cat", "bird", "dog", "rabbit", "reptile"];
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 const SearchParams = () => {
-  const [location, setLocation] = useState("");
-  const [animal, setAnimal] = useState("");
-  const [breed, setBreed] = useState("");
+  const [animal, updateAnimal] = useState("");
+  const [location, updateLocation] = useState("");
+  const [breed, updateBreed] = useState("");
   const [pets, setPets] = useState([]);
-  const breeds = useBreedList(animal);
+  const [breeds] = useBreedList(animal);
 
   useEffect(() => {
     requestPets();
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   async function requestPets() {
-    const response = await fetch(
+    const res = await fetch(
       `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
     );
-    const json = await response.json();
+    const json = await res.json();
 
     setPets(json.pets);
   }
@@ -36,46 +36,44 @@ const SearchParams = () => {
           Location
           <input
             id="location"
-            onChange={(event) => setLocation(event.target.value)}
             value={location}
             placeholder="Location"
+            onChange={(event) => updateLocation(event.target.value)}
           />
         </label>
-
         <label htmlFor="animal">
           Animal
           <select
             id="animal"
             value={animal}
-            onChange={(event) => setAnimal(event.target.value)}
-            onBlur={(event) => setAnimal(event.target.value)}
+            onChange={(event) => updateAnimal(event.target.value)}
+            onBlur={(event) => updateAnimal(event.target.value)}
           >
             <option />
             {ANIMALS.map((animal) => (
-              <option value={animal} key={animal}>
+              <option key={animal} value={animal}>
                 {animal}
               </option>
             ))}
           </select>
         </label>
-
         <label htmlFor="breed">
           Breed
           <select
+            disabled={!breeds.length}
             id="breed"
             value={breed}
-            onChange={(event) => setBreed(event.target.value)}
-            onBlur={(event) => setBreed(event.target.value)}
+            onChange={(event) => updateBreed(event.target.value)}
+            onBlur={(event) => updateBreed(event.target.value)}
           >
-            <option value=""></option>
+            <option />
             {breeds.map((breed) => (
-              <option value={breed} key={breed}>
+              <option key={breed} value={breed}>
                 {breed}
               </option>
             ))}
           </select>
         </label>
-
         <button>Submit</button>
       </form>
       <Results pets={pets} />
